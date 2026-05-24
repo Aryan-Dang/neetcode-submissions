fd0aceb94f879e30f2b7798983d940e412cd5e69
Aryan <67460333+Aryan-Dang@users.noreply.github.com>
2026-05-24 16:31:20 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        def countChars(string : str) -> str:
+            stringCounter = [0]*26
+            for c in string : #c is a str!
+                index = ord(c) - 97
+                stringCounter[index] += 1 
+            return stringCounter
+        
+        return countChars(s) == countChars(t)
