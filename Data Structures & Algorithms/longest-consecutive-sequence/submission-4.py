6dbb070cd1d3e2fbd1e95627ee65fa0b5fb0f807
Aryan <67460333+Aryan-Dang@users.noreply.github.com>
2026-05-25 14:28:27 -0400

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    """
+    array of ints -> len longest consecutive sequence
+    do not have to be consequtive in the original array
+    O(N)
+    - not first n numbers otherwise could have used integer sum property
+    - O(N) to maintain a set, set lookup is O(1), if appending to a set is a problem initialize a set to a larger num?
+
+    so initial thought is to take the biggest num in the list and iterate backwards from that/check if the number before that is in their and then 
+
+    how to handle duplicates would be a followup/check examples -> dupes don't matter, ignore
+
+    turns out max(nums) doesn't work on empty lists... so special edge case handling think about base case like empty lists etc...
+
+    didn't handle negative nums!! we didn't think about negative nums here...
+    iterate from min to max...
+    """
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        seenNums = set(nums) # O(N)
+        maxNum = max(nums)
+        minNum = min(nums)
+        longestCount = 1
+        curCount = 1
+        for i in range(minNum, maxNum+1, 1):
+            if i+1 in seenNums:
+                curCount+=1
+            else:
+                curCount = 0
+            longestCount = max(curCount, longestCount)
+        return longestCount
+
+
+        
