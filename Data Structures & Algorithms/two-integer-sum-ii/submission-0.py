3026e87208737fbcf07767638578604b4ec5b80b
Aryan <67460333+Aryan-Dang@users.noreply.github.com>
2026-05-26 16:15:39 -0400

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    """
+    sorted in non-decreasing order means sorted in increasing order but dupes might exist...
+
+    cannot use the same element twice indices can't be equal so be sure to stop before they're equal!
+
+    how does 1-indexed matter here? what about dupes here?
+
+    """
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i,j = 0, len(numbers) - 1
+
+        while i < j :
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            
+            if numbers[i] + numbers[j] > target:
+                j -= 1
+            if numbers[i] + numbers[j] < target:
+                i += 1
+        
+        return []
+            
