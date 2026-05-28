f91b7e1dd8142426881dc092cfceb58edaf12d37
Aryan <67460333+Aryan-Dang@users.noreply.github.com>
2026-05-28 15:32:57 -0400

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+intuitive solution seems to be DFS and just maintain max length found at each node... and then just comprae the length
+"""
+
+class Solution:
+
+    def calculateSubTreeLength(self, root : Optional[TreeNode], curLen) -> int:
+        maxLen = curLen
+        if root is not None:
+            print(curLen)
+            curLen += 1
+            leftLen = self.calculateSubTreeLength(root.left, curLen)
+            rightLen = self.calculateSubTreeLength(root.right, curLen)
+            maxLen = max(leftLen, rightLen)
+        return maxLen
+
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return self.calculateSubTreeLength(root, 0)
+        
