c6a459de91d9abc3444af0c99a6aefa0787ac0ef
Aryan <67460333+Aryan-Dang@users.noreply.github.com>
2026-06-03 16:44:11 -0400

    Add: min-cost-climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    '''
+    seems like a copy of the prev. problem basically
+    use the description to come up with recurrence
+    '''
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        ways = [0 for i in range(n+2)]
+        for i in range(2,n+2):
+            ways[i] = cost[i-2] + min(ways[i-1], ways[i-2])
+        return min(ways[n], ways[n+1])
